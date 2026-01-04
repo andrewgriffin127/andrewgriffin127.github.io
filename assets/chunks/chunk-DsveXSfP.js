9c9cac2fb82f7218460dab4818f6af1b34fbeb21
andrewgriffin127 <andrewgriffin127@gmail.com>
2026-01-04 12:49:03 -0600

    Version 26.1.4.1041

---
+++ assets/chunks/chunk-DsveXSfP.js
@@ -0,0 +1,1 @@
+import{r as e,E as s}from"./chunk-BfBjeSX5.js";const n="h1",m=e.forwardRef(({tag:a,...r},t)=>{const o=s(r)??a??n;return e.createElement(o,{...r,ref:t})});m.displayName="Heading";export{m as d};
